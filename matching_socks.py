47bc18437145b1746d36c360b0c722448f8ce026
Mauricio O <mauricio.ochoaen@udlap.mx>
2020-04-01 22:38:38 -0600

    socks in images and images of all socks

--- matching_socks.py
+++ matching_socks.py
@@ -4,10 +4,11 @@ sort them automatically
 """
 import random #IMPORT random to generate the random list of socks
 import pygame #IMPORT PYGAME for the grapchical version of the code.
+import os
 #Initialize pygame
 pygame.init()
 #size of the screen
-size = 800, 600
+size = 1200, 900
 #pygame clock
 clock = pygame.time.Clock()
 #available list of socks 
@@ -36,8 +37,15 @@ pygame.display.set_caption("SOCK SORTING")
 amount_of_socks =   random.randint(2,len(socks)) #random amount of socks (minimum 2 pairs and a max length of socks)
 run = True
 #for GUI purposes define socks
-blue_sock = pygame.image.load('blue.png')
-
+blue_sock = pygame.image.load(os.path.join('images/blue.png'))
+orange_sock = pygame.image.load(os.path.join('images/orange.png'))
+yellow_sock = pygame.image.load(os.path.join('images/yellow.png'))
+red_sock = pygame.image.load(os.path.join('images/red.png'))
+green_sock = pygame.image.load(os.path.join('images/green.png'))
+white_sock = pygame.image.load(os.path.join('images/white.png'))
+black_sock = pygame.image.load(os.path.join('images/black.png'))
+striped_blue_sock = pygame.image.load(os.path.join('images/striped_blue.png'))
+striped_orange_sock = pygame.image.load(os.path.join('images/striped_orange.png'))
 #switch case to python
 def blue():
     blue_list.append("blue")
@@ -47,41 +55,49 @@ def blue():
 
 def orange():
     orange_list.append("orange")
+    screen.blit(orange_sock, (random.randint(0,800),random.randint(0,600)))
     if orange_list not in drawer:
         drawer.append(orange_list)
     
 def yellow():
     yellow_list.append("yellow")
+    screen.blit(yellow_sock, (random.randint(0,800),random.randint(0,600)))
     if yellow_list not in drawer:
         drawer.append(yellow_list)
     
 def red():
     red_list.append("red")
+    screen.blit(red_sock, (random.randint(0,800),random.randint(0,600)))
     if red_list not in drawer:
         drawer.append(red_list)
     
 def black():
     black_list.append("black")
+    screen.blit(black_sock, (random.randint(0,800),random.randint(0,600)))
     if black_list not in drawer:
         drawer.append(black_list)
     
 def white_fu():
     white_list.append("white")
+    screen.blit(white_sock, (random.randint(0,800),random.randint(0,600)))
     if white_list not in drawer:
         drawer.append(white_list)
     
 def striped_blue():
     striped_blue_list.append("striped blue")
+    screen.blit(striped_blue_sock, (random.randint(0,800),random.randint(0,600)))
     if striped_blue_list not in drawer:
         drawer.append(striped_blue_list)
     
 def striped_orange():
     striped_orange_list.append("striped orange")
+    screen.blit(striped_orange_sock, (random.randint(0,800),random.randint(0,600)))
     if striped_orange_list not in drawer:
         drawer.append(striped_orange_list)
     
 def green():
     green_list.append("green")
+    screen.blit(green_sock, (random.randint(0,800),random.randint(0,600)))
     if green_list not in drawer:
         drawer.append(green_list)
 
